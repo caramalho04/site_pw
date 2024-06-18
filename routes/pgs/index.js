905be30e037628ac1aeaeb910dfbddea367f437a
Francisco  Caramalho <franciscocaramalho@ipvc.pt>
2024-06-19 00:57:19 +0100

    teste5

--- routes/pgs/index.js
+++ routes/pgs/index.js
@@ -2,6 +2,21 @@ const router = require('express').Router();
 const projetosRouter = require('./projetos');
 const authRouter = require('./auth');
 
+const router1 = createBrowserRouter  ([
+    {
+        path: "/",
+        element: Home(),
+    },
+    {
+        path:'/homepage',
+        element: Teste()
+    }
+])
+
+    
+
+
+
 router.use('/auth', authRouter);
 router.use('/projetos', projetosRouter);
 
