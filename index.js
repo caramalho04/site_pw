2c45f7f15a6f79e87067fc7edd5a0bdd86a6fb91
Francisco  Caramalho <franciscocaramalho@ipvc.pt>
2024-06-19 00:36:49 +0100

    teste2

--- index.js
+++ index.js
@@ -2,20 +2,10 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Definindo a pasta onde os arquivos estáticos (HTML, CSS, JS, etc.) estão localizados
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Rota para redirecionar /frontEnd/homepage.html para homepage.html
-app.get('/frontEnd/homepage.html', (req, res) => {
-    res.redirect('/homepage.html');
-});
-
-// Rota para a raiz do servidor, redirecionando para homepage.html
 app.get('/', (req, res) => {
-    res.redirect('/homepage.html');
+ res.send('Hello World! IPVC DEMO!  Realizado Por Francisco Caramalho Pedro Carvalho e João Vieira...');
 });
-
 
 app.listen(port, () => {
- console.log(`App listening at http://localhost:4242`);
+ console.log(`App listening at http://localhost:${port}`);
 });
