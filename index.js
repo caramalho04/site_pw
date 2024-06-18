b3b897bb0fa6c946b75705c784835aee02e58a70
Francisco  Caramalho <franciscocaramalho@ipvc.pt>
2024-06-19 00:59:39 +0100

    teste22

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
- res.send('Hello World! IPVC DEMO!  Atualizado por Francisco Caramalho, Joao Vieira, Pedro Carvalho...');
-});
+    res.redirect('/homepage');
+  });
 
 app.listen(port, () => {
  console.log(`App listening at http://localhost:${port}`);
