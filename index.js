cc3545019afadf774ec0066a2d133369470d81e2
Francisco  Caramalho <franciscocaramalho@ipvc.pt>
2024-06-19 00:29:29 +0100

    123

--- index.js
+++ index.js
@@ -3,8 +3,13 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 4242;
 
-// Define a pasta onde os arquivos estáticos (HTML, CSS, JS, etc.) estão localizados
+// Defina a pasta onde os arquivos estáticos (HTML, CSS, JS, etc.) estão localizados
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Rota para a raiz do servidor, redirecionando para homepage.html
+app.get('/', (req, res) => {
+    res.redirect('/homepage.html');
+});
 
 // Rota para redirecionar para homepage.html
 app.get('/frontEnd/homepage.html', (req, res) => {
