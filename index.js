63c5342862a5510ba9f3c1c1f948997620f1e2bf
Francisco  Caramalho <franciscocaramalho@ipvc.pt>
2024-06-19 01:05:30 +0100

    teste20

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Servindo o arquivo homepage.html
+app.get('/homepage.html', (req, res) => {
+  res.sendFile(__dirname + '/homepage.html');
+});
+
+// Configurando o redirecionamento para /
 app.get('/', (req, res) => {
-    res.redirect('/homepage');
-  });
+  res.redirect('/homepage.html');
+});
 
 app.listen(port, () => {
- console.log(`App listening at http://localhost:${port}`);
+  console.log(`App listening at http://localhost:${port}`);
 });
